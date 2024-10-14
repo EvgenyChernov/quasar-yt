32dd19853aa7e36443f29b534aa3c6b52abb0838
EvgenyChernov <evgenycher89@gmail.com>
2024-10-14 17:37:42 +1200

    сделан счетчик простой

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-img
+        src="p1.jpg"
+        class="fit absolute"
+    />
+    <q-page-container>
+      <router-view/>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.q-page {
+  max-width: 600px;
+  margin: 0 auto;
+}
+</style>/
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,62 @@
+<template>
+  <q-page class="flex flex-center text-white text-bold">
+    <div class="row">
+      <q-input
+          input-class="text-center text-h5 text-white"
+          color="teal"
+          filled
+          placeholder="Placeholder"
+          v-model="data.counter"
+      />
+    </div>
+    <div class="row full-width items-center">
+      <div class="col text-center">
+        <q-btn
+            round
+            color="primary"
+            size="xl"
+            icon="remove"
+            @click="remove"
+        />
+      </div>
+      <div class="col text-center text-h2">{{data.counter}}</div>
+      <div class="col text-center">
+        <q-btn round
+               color="primary"
+               size="xl"
+               icon="add"
+               @click="add"
+        />
+      </div>
+    </div>
+    <div class="row">
+      <q-btn
+          round
+          color="primary"
+          size="xl"
+          icon="restart_alt"
+          @click="setValueInit"
+      />
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import {reactive} from "vue";
+
+const data = reactive({
+  counter: 0,
+})
+
+const add = () => {
+  data.counter++;
+}
+const remove = () => {
+  data.counter--;
+}
+const setValueInit = () => {
+  data.counter = 0;
+}
+
+
+</script>
