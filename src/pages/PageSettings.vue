cff6df12954fd6d068f7b0bf5c1c17eabc43aaf9
evgen <evgenycher89@gmail.com>
2024-10-19 19:40:47 +1200

    добавление тогла на страницу настроек

--- src/pages/PageSettings.vue
+++ src/pages/PageSettings.vue
@@ -1,9 +1,30 @@
 <template>
-  <q-page class="flex flex-center">
-    Sittings
+  <q-page>
+    <div class="q-pa-md">
+      <q-list bordered padding>
+
+        <q-item-label header>Записи</q-item-label>
+
+        <q-item tag="label" v-ripple>
+          <q-item-section>
+            <q-item-label>Подтверждение удаления</q-item-label>
+          </q-item-section>
+          <q-item-section side>
+            <q-toggle
+              v-model="storeSetting.settings.promptToDelete"
+            />
+          </q-item-section>
+        </q-item>
+
+      </q-list>
+    </div>
   </q-page>
 </template>
 
 <script setup lang="ts">
+
+import {useStoreSetting} from "stores/storeSetting";
+
+const storeSetting = useStoreSetting()
 
 </script>
