07cc9aff1e6ca7118329a42141737444b0086df7
evgen <evgenycher89@gmail.com>
2024-10-23 11:41:10 +1200

    изменил структуру запроса к базе и хранение по связи с пользователем

--- src/stores/storeEntries.ts
+++ src/stores/storeEntries.ts
@@ -4,7 +4,7 @@ import {Notify} from "quasar";
 import {collection, onSnapshot, addDoc, doc, deleteDoc, updateDoc} from "firebase/firestore";
 import {db} from "src/firebase/firebase"
 
-const entriesCollectionRef = collection(db, "entries")
+const entriesCollectionRef = collection(db, "users", 'xGV6k98LkaRoYJWhUhLsMXKxiTs2', 'entries')
 
 export const useStoreEntries = defineStore('entries', () => {
 
