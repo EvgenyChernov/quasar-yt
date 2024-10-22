4e588a7edbddf70fb572be266bc3bc638369da46
evgen <evgenycher89@gmail.com>
2024-10-22 16:36:41 +1200

    подготовка формы входа

--- src/pages/PageAuth.vue
+++ src/pages/PageAuth.vue
@@ -3,12 +3,26 @@
     <q-card
       class="auth bg-primary text-white q-pa-lg"
     >
-      <ToolbarTitle/>
+      <q-card-section>
+        <ToolbarTitle/>
+      </q-card-section>
+      <q-card-section>
+        <q-tabs
+          v-model="tab"
+          no-caps
+        >
+          <q-tab name="login" label="Вход"/>
+          <q-tab name="register" label="Регистрация"/>
+        </q-tabs>
+      </q-card-section>
     </q-card>
   </q-page>
 </template>
 
 <script setup lang="ts">
+import {ref} from 'vue'
+
+const tab = ref('login')
 
 import ToolbarTitle from "components/Layout/ToolbarTitle.vue";
 </script>
