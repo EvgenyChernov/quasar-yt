46f05b1bb8fa98b6b3641b55bf6b392964a187d0
evgen <evgenycher89@gmail.com>
2024-10-22 19:24:52 +1200

    добавил модуль авторизации firebase и создал стор

---
+++ src/stores/storeAuth.ts
@@ -0,0 +1,25 @@
+import {defineStore} from "pinia";
+import {auth} from "src/firebase/firebase";
+
+
+export const useStoreAuth = defineStore('auth', () => {
+  //state
+
+
+  //getters
+
+
+  //actions
+
+
+  return {
+    //state
+
+
+    //getters
+
+
+    //actions
+
+  }
+})
